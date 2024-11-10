44ab239f2bb2898095451ef242b459e68739fa2d
Manu Evans <turkeyman@gmail.com>
2024-11-10 21:15:34 +1000

    Improve EZSP client implementation.  - also support fibres!

--- src/protocol/ezsp/client.d
+++ src/protocol/ezsp/client.d
@@ -1,6 +1,7 @@
 module protocol.ezsp.client;
 
 import urt.endian;
+import urt.fibre;
 import urt.lifetime;
 import urt.log;
 import urt.map;
@@ -18,6 +19,8 @@ import protocol.ezsp.ashv2;
 
 public import protocol.ezsp.commands;
 
+//version = DebugMessageFlow;
+
 nothrow @nogc:
 
 
@@ -27,55 +30,136 @@ nothrow @nogc:
 //
 
 
+enum IsEZSPRequest(T) = is(typeof(T.Command) : ushort) && is(T.Request == struct) && is(T.Response == struct);
+
+template EZSPArgs(T)
+{
+    static assert(IsEZSPRequest!T, T.stringof ~ " is not an EZSP command structure");
+    alias EZSPArgs = typeof(T.Request.tupleof);
+}
+
+template EZSPResult(T)
+{
+    static assert(IsEZSPRequest!T, T.stringof ~ " is not an EZSP command structure");
+    static if (T.Response.tupleof.length == 0)
+        alias EZSPResult = void;
+    else static if (T.Response.tupleof.length == 1)
+        alias EZSPResult = typeof(T.Response.tupleof[0]);
+    else
+        alias EZSPResult = T.Response;
+}
+
 class EZSPClient : BaseObject
 {
     __gshared Property[1] Properties = [ Property.create!("stream", stream)() ];
-nothrow @nogc:
+@nogc:
 
     enum TypeName = StringLit!"ezsp";
 
-    this(String name, ObjectFlags flags = ObjectFlags.None)
+    enum StackType : ubyte
+    {
+        Unknown = 0,
+        Router = 1,
+        Coordinator = 2
+    }
+
+    this(String name, ObjectFlags flags = ObjectFlags.None) nothrow
     {
         super(collectionTypeInfo!EZSPClient, name.move, flags);
     }
 
     // Properties...
 
-    final inout(Stream) stream() inout pure
+    final inout(Stream) stream() inout pure nothrow
         => ash.stream;
-    final void stream(Stream stream)
+    final void stream(Stream stream) nothrow
     {
         if (ash.stream !is stream)
         {
             // rebuild ASH and reset
             ash = ASH(stream);
+            ash.setEventCallback(&eventCallback);
             ash.setPacketCallback(&incomingPacket);
             restart();
         }
     }
 
-
     // API...
 
-    final void setMessageHandler(void delegate(ubyte sequence, ushort command, const(ubyte)[] message) nothrow @nogc callback)
+    final void reboot() nothrow
+    {
+        sendCommand!EZSP_ResetNode(null);
+        restart();
+    }
+
+    EZSPResult!R request(R)(auto ref EZSPArgs!R args)
+        if (IsEZSPRequest!R)
+    {
+        import urt.util : InPlace, Default;
+
+        assert(isInFibre(), "EZSPClient.request() must be called from a fibre context");
+
+        struct Result
+        {
+            YieldEZSP e;
+            static if (!is(EZSPResult!R == void))
+                EZSPResult!R result;
+        }
+
+        Result r;
+        auto ev = InPlace!YieldEZSP(Default);
+        r.e = ev;
+
+        static void response(void* userData, typeof(R.Response.tupleof) results)
+        {
+            Result* r = cast(Result*)userData;
+            r.e.finished = true;
+            enum numResults = R.Response.tupleof.length;
+            static if (numResults > 1)
+            {
+                static foreach (i; 0 .. numResults)
+                    r.result.tupleof[i] = results[i];
+            }
+            else static if (numResults == 1)
+                r.result = results[0];
+        }
+
+        bool success = sendCommand!R(&response, forward!args, cast(void*)&r);
+        assert(success, "EZSP: failed to send command!");
+
+        yield(ev);
+
+        static if (!is(EZSPResult!R == void))
+            return r.result;
+    }
+
+nothrow:
+    final void setMessageHandler(void delegate(ubyte, ushort, const(ubyte)[]) nothrow @nogc callback) pure nothrow
     {
         messageHandler = callback;
     }
 
     template setCallbackHandler(EZSP_Command)
+        if (IsEZSPRequest!EZSP_Command)
     {
-        static assert(is(typeof(EZSP_Command.Command) : ushort) && is(EZSP_Command.Response == struct),
-                      EZSP_Command.stringof ~ " is not an EZSP command structure");
-
         alias ResponseParams = typeof(EZSP_Command.Response.tupleof);
 
-        final void setCallbackHandler(Callback)(Callback responseHandler, void* userData = null)
+        final void setCallbackHandler(Callback)(Callback responseHandler, void* userData = null) nothrow
         {
             alias Args = Parameters!Callback;
             static assert (is(Args == ResponseParams) || is(Args == AliasSeq!(void*, ResponseParams)), "Callback must be a function with arguments matching " ~ EZSP_Command.stringof ~ ".Response, and optionally a `void* userData` argument in the first position.");
             enum HasUserData = Args.length > 0 && is(Args[0] == void*);
 
-            auto handler = &commandHandlers.insert(EZSP_Command.Command, CommandHandler());
+            version(DebugMessageFlow)
+            {
+                import urt.string.format;
+                if ((EZSP_Command.Command in commandNames) is null)
+                    commandNames.insert(EZSP_Command.Command, CommandData(EZSP_Command.stringof[5 .. $],
+                                                                          (const(ubyte)[] data){ EZSP_Command.Request r; data.ezspDeserialise(r); return tconcat(r); },
+                                                                          (const(ubyte)[] data){ EZSP_Command.Response r; data.ezspDeserialise(r); return tconcat(r); }));
+            }
+
+            auto handler = &commandHandlers.replace(EZSP_Command.Command, CommandHandler());
 
             handler.responseShim = &responseShim!(HasUserData, ResponseParams);
             handler.userData = HasUserData ? userData : null;
@@ -93,34 +177,40 @@ nothrow @nogc:
     }
 
     template sendCommand(EZSP_Command)
+        if (IsEZSPRequest!EZSP_Command)
     {
-        static assert(is(typeof(EZSP_Command.Command) : ushort) && is(EZSP_Command.Request == struct) && is(EZSP_Command.Response == struct),
-                      EZSP_Command.stringof ~ " is not an EZSP command structure");
-
         alias RequestParams = typeof(EZSP_Command.Request.tupleof);
         alias ResponseParams = typeof(EZSP_Command.Response.tupleof);
 
-        final bool sendCommand(Callback)(Callback responseHandler, RequestParams args, void* userData = null)
+        final bool sendCommand(Callback)(Callback responseHandler, auto ref RequestParams args, void* userData = null)
         {
             if (!running)
                 return false;
 
-            alias Args = Parameters!Callback;
-            static assert (is(Args == ResponseParams) || is(Args == AliasSeq!(void*, ResponseParams)), "Callback must be a function with arguments matching " ~ EZSP_Command.stringof ~ ".Response, and optionally a `void* userData` argument in the first position.");
-            enum HasUserData = Args.length > 0 && is(Args[0] == void*);
+            static if (!is(Callback == typeof(null)))
+            {
+                alias Args = Parameters!Callback;
+                static assert (is(Args == ResponseParams) || is(Args == AliasSeq!(void*, ResponseParams)), "Callback must be a function with arguments matching " ~ EZSP_Command.stringof ~ ".Response, and optionally a `void* userData` argument in the first position.");
+                enum HasUserData = Args.length > 0 && is(Args[0] == void*);
+            }
+
+            version(DebugMessageFlow)
+            {
+                import urt.string.format;
+                if ((EZSP_Command.Command in commandNames) is null)
+                    commandNames.insert(EZSP_Command.Command, CommandData(EZSP_Command.stringof[5 .. $],
+                                                                          (const(ubyte)[] data){ EZSP_Command.Request r; data.ezspDeserialise(r); return tconcat(r); },
+                                                                          (const(ubyte)[] data){ EZSP_Command.Response r; data.ezspDeserialise(r); return tconcat(r); }));
+            }
 
             ubyte[EZSP_Command.Request.sizeof] buffer = void;
-            size_t offset = 0;
-            static foreach (i; 0 .. RequestParams.length)
-            {{
-                size_t len = args[i].ezspSerialise(buffer[offset..$]);
-                assert(len != 0, "Request buffer too small! How did we miscalculate this?");
-                offset += len;
-            }}
+            EZSP_Command.Request tr = void;
+            tr.tupleof[] = args[];
+            size_t offset = tr.ezspSerialise(buffer[]);
 
-            writeInfof("EZSP: sending request - " ~ EZSP_Command.stringof ~ "(x{0, 04x})", EZSP_Command.Command);
-
-            static if (is_delegate!Callback)
+            static if (is(Callback == typeof(null)))
+                return sendCommandImpl(EZSP_Command.Command, buffer[0..offset], null, null, null, null);
+            else static if (is_delegate!Callback)
                 return sendCommandImpl(EZSP_Command.Command, buffer[0..offset], &responseShim!(HasUserData, ResponseParams), responseHandler.funcptr, responseHandler.ptr, HasUserData ? userData : null);
             else
                 return sendCommandImpl(EZSP_Command.Command, buffer[0..offset], &responseShim!(HasUserData, ResponseParams), responseHandler, null, HasUserData ? userData : null);
@@ -156,11 +246,21 @@ nothrow @nogc:
         buffer[i .. i + data.length] = data[];
         i += data.length;
 
+        version(DebugMessageFlow)
+        {
+            CommandData* cmdName = cmd in commandNames;
+            writeDebugf("EZSP: --> [{0}] - {1}(x{2, 04x}) - {3,?5}{4,!5}", sequenceNumber, cmdName ? cmdName.name : "UNKNOWN", cmd, cmdName ? cmdName.reqFmt(buffer[5..i]) : null, cast(void[])buffer[0..i], cmdName !is null);
+        }
+
         if (!ash.send(buffer[0..i]))
         {
+            version(DebugMessageFlow)
+                writeDebug("EZSP: send failed!");
+
             // error?!
             return -1;
         }
+
         return sequenceNumber++;
     }
 
@@ -203,7 +303,7 @@ nothrow @nogc:
 
     final override void update()
     {
-        if (!enabled || !validate())
+        if (!validate())
             return;
 
         ash.update();
@@ -231,6 +331,13 @@ nothrow @nogc:
 
 private:
 
+    static class YieldEZSP : AwakenEvent
+    {
+        nothrow @nogc:
+        bool finished;
+        override bool ready() { return finished; }
+    }
+
     struct ActiveRequests
     {
         void function(const(ubyte)[], void*, void*, void*) nothrow @nogc responseShim;
@@ -252,10 +359,10 @@ private:
 
     MonoTime lastEvent;
 
-    bool enabled = true;
-
     ubyte requestedVersion;
     ubyte knownVersion;
+    StackType stackType;
+    String stackVersion;
 
     ubyte sequenceNumber;
 
@@ -265,37 +372,78 @@ private:
     Map!(ushort, CommandHandler) commandHandlers;
     ActiveRequests[16] activeRequests;
 
+    version(DebugMessageFlow)
+    {
+        struct CommandData
+        {
+            string name;
+            const(char)[] function(const(ubyte)[]) nothrow @nogc reqFmt;
+            const(char)[] function(const(ubyte)[]) nothrow @nogc resFmt;
+        }
+        Map!(ushort, CommandData) commandNames;
+    }
+
+    void eventCallback(ASH.Event event)
+    {
+        if (event == ASH.Event.Reset)
+        {
+            knownVersion = 0;
+            requestedVersion = 0;
+            sequenceNumber = 0;
+            stackType = StackType.Unknown;
+            stackVersion = null;
+            activeRequests[] = ActiveRequests();
+        }
+        else
+            assert(false, "Unhandled ASH event");
+    }
+
     void incomingPacket(const(ubyte)[] msg)
     {
-        ubyte seq = msg[0];
+//        writeWarningf("ASHv2: [!!!] empty frame received! [x{0,02x}]", control);
+
+        ubyte seq;
         ushort control;
         ushort cmd;
         if (knownVersion >= 8)
         {
-            assert(msg.length >= 5);
+            if (msg.length < 5)
+            {
+                writeWarning("EZSP: [!!!] invalid frame; frame is too short!");
+                return;
+            }
 
+            seq = msg[0];
             control = msg[1] | (msg[2] << 8);
             cmd = msg[3] | (msg[4] << 8);
             msg = msg[5 .. $];
         }
         else
         {
-            assert(msg.length >= 4);
+            if (msg.length < 3)
+            {
+                writeWarning("EZSP: [!!!] invalid frame; frame is too short!");
+                return;
+            }
 
+            seq = msg[0];
             control = msg[1];
 
-            if (msg.length >= 3 && msg[2] == 0xff)
+            if (msg[2] == 0xff)
             {
                 assert(false); // TODO: we don't understand this case!
-                if (msg.length < 4)
-                    assert(false);
+
+                if (msg.length < 5)
+                {
+                    writeWarning("EZSP: [!!!] invalid frame; frame is too short!");
+                    return;
+                }
+
                 cmd = msg[4];
                 msg = msg[5..$];
             }
             else
             {
-                assert(msg.length >= 3);
-
                 cmd = msg[2];
                 msg = msg[3..$];
             }
@@ -334,18 +482,30 @@ private:
                     break;
                 }
 
+                import urt.string.format : tformat;
+                import urt.mem.allocator : defaultAllocator;
+
+                stackType = cast(StackType)r.stackType;
+                stackVersion = tformat("{0}.{1}.{2}.{3}", r.stackVersion >> 12, (r.stackVersion >> 8) & 0xF, (r.stackVersion >> 4) & 0xF, r.stackVersion & 0xF).makeString(defaultAllocator());
+
                 knownVersion = requestedVersion;
                 requestedVersion = 0;
 
-                writeInfo("EZSP: agreed version: ", knownVersion);
+                writeInfof("EZSP: connected: {0} V{1} - protocol version {2}", r.stackType == 1 ? "ROUTER" : r.stackType == 2 ? "COORDINATOR" : "UNKNOWN", stackVersion, knownVersion);
                 break;
 
             case 0x0058: // invalidCommand
                 EzspStatus reason = cast(EzspStatus)msg[0];
-                writeInfo("EZSP: invalid command: ", reason);
+                writeWarning("EZSP: invalid command: ", reason);
                 break;
 
             default:
+                version(DebugMessageFlow)
+                {
+                    CommandData* cmdName = command in commandNames;
+                    writeDebugf("EZSP: <-- [{0}] - {1}(x{2,04x}) - {3,?5}{4,!5}", seq, cmdName ? cmdName.name : "UNKNOWN", command, cmdName ? cmdName.resFmt(msg) : null, cast(void[])msg, cmdName !is null);
+                }
+
                 int slot = seq & 0xF;
                 if (activeRequests[slot].responseShim != null)
                 {
@@ -359,7 +519,9 @@ private:
                     else if (messageHandler)
                         messageHandler(seq, command, msg);
                     else
-                        writeInfof("EZSP: unhandled message - seq: {0}  cmd: x{1,04x} - {2}", seq, command, cast(void[])msg);
+                    {
+                        // TODO: unhandled message?
+                    }
                 }
                 break;
         }
@@ -393,11 +555,16 @@ void responseShim(bool withUserdata, Args...)(const(ubyte)[] response, void* cb,
     import urt.meta.tuple;
 
     Tuple!Args args;
-    size_t taken = response.ezspDeserialise(args);
-    if (taken != response.length)
+    static if (Args.length > 0)
     {
-        writeWarning("EZSP: error deserialising response");
-        return;
+        size_t taken = response.ezspDeserialise(args);
+        if (taken == 0 || taken > response.length)
+        {
+            writeWarning("EZSP: error deserialising response");
+            return;
+        }
+        if (taken < response.length)
+            writeWarning("EZSP: response buffer contains more bytes than expected! tail bytes: ", cast(void[])response[taken .. $]);
     }
 
     if (inst)
@@ -418,4 +585,3 @@ void responseShim(bool withUserdata, Args...)(const(ubyte)[] response, void* cb,
             (cast(void function(Args) nothrow @nogc)cb)(args.expand);
     }
 }
-
