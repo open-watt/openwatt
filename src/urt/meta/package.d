7c5d32946ebe8b4e9ce342992c567fe64db8ec76
Manu Evans <turkeyman@gmail.com>
2024-11-11 23:28:42 +1000

    Added urt.crc to centralise the growing number of CRC implementations around the place...

--- src/urt/meta/package.d
+++ src/urt/meta/package.d
@@ -6,6 +6,26 @@ alias Alias(T) = T;
 
 alias AliasSeq(TList...) = TList;
 
+template intForWidth(size_t width, bool signed = false)
+{
+    static if (width <= 8 && !signed)
+        alias intForWidth = ubyte;
+    else static if (width <= 8 && signed)
+        alias intForWidth = byte;
+    else static if (width <= 16 && !signed)
+        alias intForWidth = ushort;
+    else static if (width <= 16 && signed)
+        alias intForWidth = short;
+    else static if (width <= 32 && !signed)
+        alias intForWidth = uint;
+    else static if (width <= 32 && signed)
+        alias intForWidth = int;
+    else static if (width <= 64 && !signed)
+        alias intForWidth = ulong;
+    else static if (width <= 64 && signed)
+        alias intForWidth = long;
+}
+
 template staticMap(alias fun, args...)
 {
     alias staticMap = AliasSeq!();
