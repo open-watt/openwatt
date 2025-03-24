d881f9c5a652baed8e4df377ae6635e558238c5c
Manu Evans <turkeyman@gmail.com>
2025-03-24 22:56:53 +1000

    Add widening and narrowing functions to urt.math, also pad out support for 32-bit architectures.

--- src/urt/intrinsic/package.d
+++ src/urt/intrinsic/package.d
@@ -11,6 +11,21 @@ version (GNU)
 
 version (LDC)
 {
+    public import ldc.llvmasm;
+
+    struct RO32 { uint r; bool c; }
+    struct RO64 { ulong r; bool c; }
+
+    pragma(LDC_intrinsic, "llvm.uadd.with.overflow.i32")
+    RO32 _llvm_add_overflow(uint, uint) pure @safe;
+    pragma(LDC_intrinsic, "llvm.uadd.with.overflow.i64")
+    RO64 _llvm_add_overflow(ulong, ulong) pure @safe;
+
+    pragma(LDC_intrinsic, "llvm.usub.with.overflow.i32")
+    RO32 _llvm_sub_overflow(uint, uint) pure @safe;
+    pragma(LDC_intrinsic, "llvm.usub.with.overflow.i64")
+    RO64 _llvm_sub_overflow(ulong, ulong) pure @safe;
+
     pragma(LDC_intrinsic, "llvm.bswap.i16")
     ushort __builtin_bswap16(ushort) pure @safe;
     pragma(LDC_intrinsic, "llvm.bswap.i32")
