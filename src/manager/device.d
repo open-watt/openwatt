4b9b53ba787104ae0967c6aa2522f095dbeba7d3
Manu Evans <turkeyman@gmail.com>
2026-02-02 02:03:34 +1000

    Profiles can define element expressions.

--- src/manager/device.d
+++ src/manager/device.d
@@ -7,8 +7,10 @@ import urt.string;
 import urt.time;
 import urt.variant;
 
+import manager;
 import manager.component;
 import manager.element;
+import manager.expression;
 import manager.profile;
 import manager.sampler;
 
@@ -28,6 +30,15 @@ nothrow @nogc:
         super(id.move);
     }
 
+    ~this()
+    {
+        clear_expression_elements();
+        if (profile)
+            g_app.allocator.freeT(profile);
+    }
+
+    Profile* profile;
+    Array!ExpressionElement expressions;
     Array!Sampler samplers;
 
     bool finalise()
@@ -45,6 +56,22 @@ nothrow @nogc:
 //        last_poll = getTime();
 
         return true;
+    }
+
+    void clear_expression_elements()
+    {
+        foreach (ref e; expressions)
+        {
+            bool have_var_refs;
+            Array!(const(char)[]) refs = e.expression.get_element_refs(have_var_refs);
+            foreach (r; refs)
+            {
+                Element* el = find_element(r);
+                el.remove_subscriber(&e.element_updated);
+            }
+            e.expression.free_expression();
+        }
+        expressions.clear();
     }
 
     void update()
@@ -103,6 +130,23 @@ nothrow @nogc:
 
     Array!(Element*) sample_elements;
     MonoTime last_poll;
+
+private:
+
+    struct ExpressionElement
+    {
+        Device device;
+        Expression* expression;
+        Element* element;
+
+    nothrow @nogc:
+        void element_updated(ref Element, ref const Variant, SysTime timestamp)
+        {
+            EvalContext ctx;
+            ctx.root = device;
+            element.value(expression.evaluate(ctx), timestamp);
+        }
+    }
 }
 
 Device create_device_from_profile(ref Profile profile, const(char)[] model, const(char)[] id, const(char)[] name, scope CreateElementHandler create_element_handler)
@@ -185,12 +229,38 @@ Device create_device_from_profile(ref Profile profile, const(char)[] model, cons
 
             final switch (el.type) with (ElementTemplate.Type)
             {
-                case constant:
-                    // TODO: we should parse the value string with the expression parser...
-                    Variant v;
-                    v.fromString(el.get_constant_value(profile));
-                    e.value = v.move;
-                    e.sampling_mode = SamplingMode.constant;
+                case expression:
+                    Expression* expr;
+                    const(char)[] expr_str = el.get_expression(profile);
+                    try
+                        expr = parse_expression(expr_str);
+                    catch (Exception e)
+                    {
+                        writeWarning("Failed to parse expression: ", expr_str);
+                        goto fail;
+                    }
+
+                    bool have_var_refs;
+                    Array!(const(char)[]) refs = expr.get_element_refs(have_var_refs);
+                    if (have_var_refs)
+                    {
+                        writeWarning("Element expressions can't have variable references: ", expr_str);
+                        goto fail;
+                    }
+
+                    if (refs.empty)
+                    {
+                        EvalContext ctx;
+                        e.value = expr.evaluate(ctx);
+                        e.sampling_mode = SamplingMode.constant;
+
+                        expr.free_expression();
+                    }
+                    else
+                    {
+                        device.expressions ~= Device.ExpressionElement(device, expr, e);
+                        e.sampling_mode = SamplingMode.dependent;
+                    }
                     break;
 
                 case map:
@@ -199,6 +269,10 @@ Device create_device_from_profile(ref Profile profile, const(char)[] model, cons
             }
 
             c.elements ~= e;
+            continue;
+
+        fail:
+            g_app.allocator.freeT(e);
         }
 
         return c;
@@ -212,6 +286,26 @@ Device create_device_from_profile(ref Profile profile, const(char)[] model, cons
 
         Component c = create_component(ct);
         device.components ~= c;
+    }
+
+    // hookup expressions
+    outer: foreach (ref expr; device.expressions)
+    {
+        bool _;
+        Array!(const(char)[]) refs = expr.expression.get_element_refs(_);
+        foreach (r; refs)
+        {
+            if (!device.find_element(r))
+            {
+                writeWarning("Failed to resolve element references in expression: @", r);
+                break outer;
+            }
+        }
+        foreach (r; refs)
+        {
+            Element* e = device.find_element(r);
+            e.add_subscriber(&expr.element_updated);
+        }
     }
 
     g_app.devices.insert(device.id, device);
