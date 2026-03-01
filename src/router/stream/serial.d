9acebd0205f839c100ac6ea0f8bd294022c56ce2
Manu Evans <turkeyman@gmail.com>
2026-03-02 04:14:44 +1000

    Strip druntime for real!

--- src/router/stream/serial.d
+++ src/router/stream/serial.d
@@ -17,7 +17,7 @@ public import router.stream;
 
 version (Windows)
 {
-    import core.sys.windows.windows;
+    import urt.internal.sys.windows;
 }
 else version(Posix)
 {
@@ -175,7 +175,7 @@ nothrow @nogc:
             if (_h_com == INVALID_HANDLE_VALUE)
                 return CompletionStatus.error;
 
-            DCB dcb;
+            DCB dcb = void;
             ZeroMemory(&dcb, DCB.sizeof);
             dcb.DCBlength = DCB.sizeof;
             if (!GetCommState(_h_com, &dcb))
@@ -272,7 +272,7 @@ nothrow @nogc:
             _fd = core.sys.posix.fcntl.open(device[].tstringz, O_RDWR | O_NOCTTY | O_NDELAY);
             if (_fd == -1)
             {
-                writeln("Failed to open device %s.\n", this.device);
+                writeln("Failed to open device ", this.device);
                 return CompletionStatus.error;
             }
 
@@ -432,7 +432,7 @@ nothrow @nogc:
                     iov[i].iov_base = cast(void*)d.ptr;
                     iov[i].iov_len = d.length;
                 }
-                bytes_written = core.sys.posix.unistd.writev(_fd, iov.ptr, cast(int)data.length);
+                bytes_written = writev(_fd, iov.ptr, cast(int)data.length);
             }
             else
                 bytes_written = core.sys.posix.unistd.write(_fd, data[0].ptr, data[0].length);
