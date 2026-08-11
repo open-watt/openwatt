d5b944cff25524c5b87208dab7a82a6c4cfccc0a
Manu Evans <turkeyman@gmail.com>
2026-08-11 16:51:26 +1000

    Add capability-gated ESP32 download reboot

--- src/manager/system.d
+++ src/manager/system.d
@@ -9,7 +9,9 @@ import urt.system;
 import urt.time;
 import urt.variant : Variant;
 
-import driver.system : system_reboot, reset_reason;
+import driver.system : has_download_mode, reset_reason, system_reboot;
+static if (has_download_mode)
+    import driver.system : system_reboot_to_bootloader;
 
 import manager : get_module;
 import manager.console.session;
@@ -231,8 +233,22 @@ void show_time(Session session)
     session.write_line(getDateTime());
 }
 
-void reboot(Session session)
+void reboot(Session session, Nullable!uint bootloader)
 {
+    if (bootloader && bootloader.value)
+    {
+        static if (has_download_mode)
+        {
+            session.write_line("reboot: entering bootloader...");
+            system_reboot_to_bootloader(bootloader.value);
+            return;
+        }
+        else
+        {
+            session.write_line("reboot: no bootloader mode on this platform");
+            return;
+        }
+    }
     session.write_line("reboot: rebooting...");
     system_reboot();
 }
